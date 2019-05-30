0c49df700ea19b996b2e71fce07d8a9ca6a66471
Mohammed Saqib <saqibm128@gmail.com>
2019-05-29 20:16:34 -0400

    added k_neighbors classifier

---
+++ k_neigbors.py
@@ -0,0 +1,67 @@
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectPercentile, chi2
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import f1_score, make_scorer
+import util_funcs
+import pandas as pd
+import numpy as np
+
+from sacred import Experiment
+ex = Experiment('k_neighbors_eeg')
+from sacred.observers import MongoObserver
+ex.observers.append(MongoObserver.create())
+
+@ex.capture
+def generate_x_y(use_1, num_files, num_workers=6):
+    data_all = util_funcs.read_all(use_1, num_workers, num_files)
+    x_data = np.vstack([instance.data.mean(axis=0) for instance in data_all])
+    y_data_strings = [instance.seizure_type for instance in data_all]
+    y_data = pd.DataFrame(
+        index=range(num_files),
+        columns=set(y_data_strings)
+        ).fillna(0)
+    for i in range(num_files):
+        y_data.loc[i, y_data_strings[i]] = 1
+    return x_data, y_data
+
+@ex.named_config
+def debug_config():
+    num_files = 200
+
+@ex.config
+def config():
+    use_1 = False
+    num_files = util_funcs.TOTAL_NUM_FILES
+    parameters = {
+        'knn__n_neighbors': [1,2,4,8,16],
+        'knn__p': [1,2]
+    }
+
+@ex.automain
+def run(parameters, num_files, use_1):
+    steps = [
+        # ('feat_sel', SelectPercentile(chi2, 20)),
+        # ('scaler', StandardScaler()),
+        ('knn', KNeighborsClassifier())
+        ]
+
+    pipeline = Pipeline(steps)
+
+    gridsearch = GridSearchCV(pipeline, parameters, cv=5, scoring = make_scorer(f1_score, average="weighted"))
+
+    x_data, y_data = generate_x_y(use_1, num_files)
+
+    X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2)
+
+    gridsearch = gridsearch.fit(X_train, y_train)
+    best_pipeline = gridsearch.best_estimator_
+
+    best_pipeline.fit(X_train, y_train)
+    y_pred = best_pipeline.predict(X_test)
+    f1_res = f1_score(y_pred, y_test, average="weighted")
+
+    print("Best F1 Score: {}".format(f1_res))
+
+    return f1_res, gridsearch
--- util_funcs.py
+++ util_funcs.py
@@ -2,8 +2,10 @@ import pickle as pkl
 import json, os
 from addict import Dict
 from pathos.multiprocessing import Pool
+import pandas as pd
+import numpy as np
 
-num_files = 2012
+TOTAL_NUM_FILES = 2012
 
 def read_config():
     return json.load(open("config.json", "rb"))
@@ -12,11 +14,13 @@ def read_preproc_1(id):
     root_path = read_config()["preprocessed_1"]
     return pkl.load(open(os.path.join(root_path, "seiz_{}.pkl".format(id)), "rb"));
 
-def read_preproc_2(id):
+def read_preproc_2(id, debug=True):
+    if id % 100 == 0 and debug:
+        print("Starting reading {}".format(id))
     root_path = read_config()["preprocessed_2"]
     return pkl.load(open(os.path.join(root_path, "seiz_{}.pkl".format(id)), "rb"));
 
-def read_all(use_1=False, num_workers=4):
+def read_all(use_1=False, num_workers=4, num_files=TOTAL_NUM_FILES):
     p = Pool(processes = num_workers)
     if use_1:
         return p.map(read_preproc_1, [i for i in range(num_files)])
@@ -24,9 +28,8 @@ def read_all(use_1=False, num_workers=4):
         return p.map(read_preproc_2, [i for i in range(num_files)])
 
 
-
 if __name__ == "__main__":
     print(read_config())
     print(read_preproc_1(1))
     print(read_preproc_2(1))
-    print(read_all())
+    print(read_all_into_df(num_files=100))
